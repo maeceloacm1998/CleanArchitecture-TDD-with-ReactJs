9349a9e22dc941e8e753b530af758f4a2043085f
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-01-14 22:07:49 -0300

    docs: comments in data folder

--- src/data/usecases/authentication/protocols/http/http-post-client.ts
+++ src/data/usecases/authentication/protocols/http/http-post-client.ts
@@ -1,3 +1,4 @@
+// Interfaces com os protocolos HTTP.
 export interface HttpPostClient {
   post(url: string): Promise<void>;
 }
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -6,8 +6,13 @@ type SutTypes ={
     httpPostClientSpy: HttpPostClientSpy
 }
 
+
+//
 const makeSut = (url :string = "any_url"): SutTypes => {
+    // MOCK - Podemos dizer que é uma requisicao FAKE para testar o método.
     const httpPostClientSpy = new HttpPostClientSpy();
+
+    // SUT - System under test (Objeto que esta sendo testado.)
     const sut = new RemoteAuthentication(url, httpPostClientSpy);
 
     return {
@@ -19,6 +24,8 @@ const makeSut = (url :string = "any_url"): SutTypes => {
 describe("RemoteAuthentication", () => {
   test("Should call HttpPostClient with correct URL", async () => {
     const url = 'other_url'
+
+    // Partner vai me retornar o metodo testado da vez e um mock
     const {httpPostClientSpy,sut} = makeSut(url);
 
     await sut.auth();
