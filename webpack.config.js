0044d1a1dc3a2af50061a9ae67047ee083fc0cda
Marcelo Antonio <maeceloacm1998@gmail.com>
2022-01-22 21:02:46 -0300

    feat: add login view

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-mdule.exports = {
+module.exports = {
   mode: "development",
   entry: "./src/main/index.tsx",
   output: {
     path: path.join(__dirname, "public/js"),
-    publicPath: "public/js",
-    fildname: "bundle.js",
+    publicPath: "/public/js",
+    filename: "bundle.js",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", "scss"],
@@ -16,35 +16,33 @@ mdule.exports = {
     },
   },
   module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        loader: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: "style-folder",
-          },
-          {
-            loader: "css-folder",
-            options: {
-                modules: true
-            }
-          },
-          {
-            loader: "sass-folder",
-          },
-        ],
-      },
-    ],
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true
+        }
+      }, {
+        loader: 'sass-loader'
+      }]
+    }]
   },
   devServer: {
-    contentBase: "./public",
-    writeToDisk: true,
+    devMiddleware: {
+      writeToDisk: true
+    },
+    static: {
+      directory: './public'
+    },
     historyApiFallback: true,
+    port: 8080
   },
   externals: {
     react: "React",
